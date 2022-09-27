754435e2b2c049d8228fb27b25e11b8dfa02729a
Th4End <111306334+Th4End@users.noreply.github.com>
2022-09-27 23:06:30 +0200

    un commentaire

---
+++ conversion.py
@@ -0,0 +1,10 @@
+#défini nombre comme un entier à entrer soit même
+print("Bonjour tapez un nombre entier")
+nombre = int(input())
+#passe de binaire à hexa à entier à binaire
+print(hex(nombre))
+print(bin(nombre))
+print(int(nombre))
+print(bin(nombre))
+print(hex(nombre))
+print(int(nombre))
